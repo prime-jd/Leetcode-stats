9185c1b6e5277a3a94c64c0db6a5b8eb2a88547b
Ravi Shankar Tripathi <120632610+prime-jd@users.noreply.github.com>
2025-01-01 18:11:49 +0000

    schedule interviews

--- schedulingInterviews.cpp
+++ schedulingInterviews.cpp
@@ -1,12 +1,10 @@
-class ScheduleInterview{
-
-};
-
 class Interviewer{
     int id;
+    int start;
     public:
-    Interviewer(int id){
+    Interviewer(int id, int start){
         this.id = id;
+        this.start = start;
     }
 };
 
@@ -34,4 +32,37 @@ class Interview{
     int endTime;
     public:
     Interview(Attendee &attendee, Interviewer &interviewer, Room &room, int startTime, int endTime): attendee(attendee),interviewer(interviewer),room(room), startTime(startTime), endTime(endTime) {}
+};
+
+class ScheduleInterview{
+    int start = 9;
+    int end = 18;
+    int breakStart =14;
+    int breakEnd = 15;
+    int limitTime = 2;
+    vector<Interviewer>interviewer;
+    vector<Room>rooms;
+    vector<int>attendees;
+
+    public:
+    void availablEInterviewer(){
+
+    }
+    void schedule(){
+       int time = start;
+       int i = 0;
+       INT J= 0
+       while(attendees.size()>0 && time+2<=end){
+            if()
+            q.push(attendees[i]);
+            if(J==interviewer.size() || k==room.size()){
+                rooms.pop();
+                interviewer.pop();
+            }else{
+                time += 2;
+
+            }
+       }
+    }
+    
 };
